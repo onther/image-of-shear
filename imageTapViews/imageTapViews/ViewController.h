c33d4ba74ba045dea792a390ddeea81fbfe039d6
onther <1047612565@qq.com>
2016-05-12 11:29:52 +0800

    1.2.2

---
+++ imageTapViews/imageTapViews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  imageTapViews
+//
+//  Created by mac on 16/4/6.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
